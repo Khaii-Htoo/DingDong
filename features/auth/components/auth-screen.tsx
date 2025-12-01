b574318ed36928daef3a10d1cba84b0e125d20b1
Khaii-Htoo <khaihtooag@gmail.com>
2025-12-01 15:00:33 +0530

    done: auth ui

---
+++ features/auth/components/auth-screen.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { authType } from "@/features/type";
+import { Github } from "lucide-react";
+import { useState } from "react";
+import { AiFillGoogleCircle } from "react-icons/ai";
+
+export default function AuthScreen() {
+  const [authState, setAuthState] = useState<authType>("singIn");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  return (
+    <div className=" h-auto md:w-[450px]">
+      <Card className="  w-full">
+        <CardHeader>
+          <CardTitle className=" text-2xl">
+            {authState === "singIn" ? "Login" : "Register"} to DingDong
+          </CardTitle>
+          <CardDescription>
+            Use your email or another service to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent className=" space-y-5">
+          <form className=" space-y-2.5">
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Email"
+              required
+            />
+            <Input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder=" password"
+              required
+            />
+            {authState === "singIn" && (
+              <Input
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type="password"
+                placeholder=" confirmPassword"
+                required
+              />
+            )}
+            <Button className=" w-full">Continue</Button>
+          </form>
+          <Separator className="my-4" />
+          <Button className=" w-full" variant={"outline"}>
+            <AiFillGoogleCircle />
+            Continue with goolge
+          </Button>
+          <Button className=" w-full" variant={"outline"}>
+            <Github />
+            Continue with github
+          </Button>
+          <p className=" text-sm text-muted-foreground">
+            Already have an account?{" "}
+            <span
+              className=" text-sky-600 cursor-pointer hover:underline"
+              onClick={() =>
+                setAuthState((prev) => (prev == "singIn" ? "singUp" : "singIn"))
+              }
+            >
+              {authState === "singIn" ? "Sign In" : "Sign up"}
+            </span>
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
